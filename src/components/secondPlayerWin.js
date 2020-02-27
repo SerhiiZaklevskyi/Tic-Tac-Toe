5dc2aec26d906cdbeb6ed02dd542ada7fe7adfba
Serhii Zaklevskyi <serhii.jh@gmail.com>
2020-02-27 09:44:40 +0200

    исправлен баг, когда при победе 2го игрока символ первого менялся на противоположный

--- src/components/secondPlayerWin.js
+++ src/components/secondPlayerWin.js
@@ -5,5 +5,8 @@ export function secondPlayerWin() {
     document.querySelector('#winner').innerText = `${store.state.playerTwo} WON!`;
     store.state.counterTwo++
     localStorage.setItem('counterTwo', store.state.counterTwo)
-    store.dispatch('resetGame', false)
+    if(store.state.firstPlayerX === false) {
+        store.dispatch('resetGame', false)
+    }
+    else store.dispatch('resetGame', true)
 }
